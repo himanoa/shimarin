0e4a726f4ed2f77b29a518086a61657bacb89fe8
himanoa <matsunoappy@gmail.com>
2018-02-18 05:54:44 +0900

    Tweet

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,40 @@
-const functions = require('firebase-functions');
-const twitter = require('twitter');
+const functions = require('firebase-functions') ;
+const Twitter = require('twitter');
 
 exports.shimarin= functions.https.onRequest((request, response) => {
+  const errorHandler = (error) => {
+    response.json({
+      status: 500,
+      payload: {
+        error: JSON.stringify(error.message)
+      },
+      error: true
+    })
+  }
   response.contentType('application/json');
-  response.json({
-    status: 200,
-    payload: {
+  try{
+    const auth = {
+      consumer_key: process.env.consumerKey || functions.config().twitter.consumerkey,
+      consumer_secret: process.env.consumerSecret || functions.config().twitter.consumersecret,
+      access_token_key: process.env.accessToken || functions.config().twitter.accesstoken,
+      access_token_secret: process.env.accessTokenSecret || functions.config().twitter.accesstokensecret
     }
-  });
+    const tw = new Twitter(auth)
+
+    tw.post('statuses/update', { status: 'ｺﾄﾉﾊｼﾏｲｶﾜｲｲﾔｯﾀ-' }, (error) =>  {
+      if(error) {
+        errorHandler(error)
+        return
+      }
+      response.json({
+        status: 200,
+        payload: {
+        },
+        error: false
+      });
+    })
+
+  }catch(e) {
+    errorHandler(e)
+  }
 });
