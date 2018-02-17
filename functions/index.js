fe92b502a0156e2848076fa146ca59dc33396ae3
himanoa <matsunoappy@gmail.com>
2018-02-18 05:02:45 +0900

    Add dependency twitter package

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,11 @@
 const functions = require('firebase-functions');
+const twitter = require('twitter');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.getsShimarin = functions.https.onRequest((request, response) => {
+exports.shimarin= functions.https.onRequest((request, response) => {
   response.contentType('application/json');
   response.json({
     status: 200,
     payload: {
-      tintin: true
     }
   });
 });
