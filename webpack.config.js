952be5d64778ae8a65b04545212b63a29e766a54
himanoa <matsunoappy@gmail.com>
2018-02-17 16:33:08 +0900

    styleload

--- webpack.config.js
+++ webpack.config.js
@@ -5,49 +5,71 @@ const webpack = require('webpack')
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const FlowWebpackPlugin = require('flow-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-module.exports = {
-  entry: {
-    app: ['./src/index.js']
-  },
-  output: {
-    filename: '[name].bundle.js',
-    chunkFilename: '[name].bundle.js',
-    path: __dirname + '/public',
-    publicPath: '/'
-  },
-  devServer: {
-    contentBase: 'public/',
-    historyApiFallback: true,
-    port: DEV_PORT
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.scss$/,
-        use: 'sass-loader'
-      }
+module.exports = [
+  {
+    entry: {
+      app: ['./src/index.js'],
+    },
+    output: {
+      filename: '[name].bundle.js',
+      chunkFilename: '[name].bundle.js',
+      path: __dirname + '/public',
+      publicPath: '/'
+    },
+    devServer: {
+      contentBase: 'public/',
+      historyApiFallback: true,
+      port: DEV_PORT
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: 'babel-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    devtool: process.env.NODE_ENV === 'production' ? null : 'inline-source-map',
+    plugins:[
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+      new FlowWebpackPlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        sourceMap: true
+      }),
+      new CleanWebpackPlugin(['public']),
+      new HtmlWebpackPlugin({
+        hash: true,
+        template: './src/index.template.ejs',
+        inject: 'body',
+      })
     ]
   },
-  devtool: process.env.NODE_ENV === 'production' ? null : 'inline-source-map',
-  plugins:[
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    }),
-    new FlowWebpackPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true
-    }),
-    new CleanWebpackPlugin(['public']),
-    new HtmlWebpackPlugin({
-      hash: true,
-      template: './src/index.template.ejs',
-      inject: 'body',
-    })
-  ]
-}
+  {
+    entry: {
+      style: __dirname + "/stylesheets/style.scss", // トランスパイル対象
+    },
+    output: {
+      path: __dirname + '/public', // 出力先ディレクトリ
+      filename: '[name].css'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          loader: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader', 'sass-loader']
+          })
+        }
+      ]
+    },
+    plugins: [
+      new ExtractTextPlugin('[name].css'),
+    ],
+  },
+]
