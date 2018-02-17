a6194d64b28b597989c7c869e3111ee0d14697a5
himanoa <matsunoappy@gmail.com>
2018-02-18 04:48:53 +0900

    Test cloud functions

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,12 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.getsShimarin = functions.https.onRequest((request, response) => {
+  response.contentType('application/json');
+  response.json({
+    status: 200,
+    payload: {
+      tintin: true
+    }
+  });
+});
