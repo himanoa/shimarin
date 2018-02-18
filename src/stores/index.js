22034a1d859184b14b73ad6cda5b9ec405b23317
himanoa <matsunoappy@gmail.com>
2018-02-19 00:23:36 +0900

    Fix

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,9 +1,8 @@
-// @flow
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import createSagaMiddleware from "redux-saga";
 import reducers from "../reducers";
 
-const sagaMiddlewares = createSagaMiddleware();
-const store = createStore(reducers, applyMiddleware(sagaMiddlewares));
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducers, compose(applyMiddleware(sagaMiddleware)));
 
 export default store;
