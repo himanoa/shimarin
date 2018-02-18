00bc4e308799b34a43b576c1c61e61ec77022e40
himanoa <matsunoappy@gmail.com>
2018-02-19 01:55:01 +0900

    Rename fetchTweet to fetchShimarin

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,8 +1,11 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import createSagaMiddleware from "redux-saga";
 import reducers from "../reducers";
+import fetchShimarin from "../sagas/fetchShimarin.js";
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(reducers, compose(applyMiddleware(sagaMiddleware)));
+
+sagaMiddleware.run(fetchShimarin);
 
 export default store;
