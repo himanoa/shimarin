3a341c9c82825cd449f0b75f3dde76e3dc21b2f9
himanoa <matsunoappy@gmail.com>
2018-02-19 01:21:38 +0900

    Fix action name

--- src/reducers/tweets.js
+++ src/reducers/tweets.js
@@ -1,11 +1,12 @@
 // @flow
-import { SYNC_TWEET } from "../actions/tweet";
+import { TWEETS_FETCH_SUCCESS, TWEETS_FETCH_FAILED } from "../actions/tweet";
 import type { Tweet, Action } from "../actions/tweet";
 
 const tweets = (state: Array<Tweet> = [], action: Action) => {
   switch (action.type) {
-    case SYNC_TWEET:
+    case TWEETS_FETCH_SUCCESS:
       return action.payload.tweets;
+    case TWEETS_FETCH_FAILED:
     default:
       return state;
   }
