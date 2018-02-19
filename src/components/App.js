5b8a1afb7510cafa874c4e285970e8e494bebb40
himanoa <matsunoappy@gmail.com>
2018-02-20 05:09:51 +0900

    Delete footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,12 @@
 /* @flow */
 import React from "react";
 import Header from "./Header";
-import Footer from "./Footer";
 import TweetList from "./TweetList";
 
 const App = () => (
   <div>
     <Header />
     <TweetList />
-    <Footer />
   </div>
 );
 
